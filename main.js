9e057f52436df346e94c896fd538e8de99ab7d95
Cameron Tremblay <catremblay33@gmail.com>
2025-11-14 08:07:16 -0500

    planning on js file comments, added projects page

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+Just planning for whole sit and I'm to lazy to make a new text file
+Home page - title, short slogan and intro
+About - explain values, what we do, etc.
+Info - member requirements, contact info, meetings, FAQs, contact w/ questions
+Coding - link to youtube channel, reccomend some resources
+Projects page? - show what our club has done/planning to do
+*/
